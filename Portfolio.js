e3ea20649120fb5f4116e25076757609fcaa3c87
AwesomeCreatorGames <74663739+AwesomeCreatorGames@users.noreply.github.com>
2021-03-26 14:31:51 +0530

    Update Portfolio.js

--- Portfolio.js
+++ Portfolio.js
@@ -79,3 +79,15 @@
 
 	
 })(jQuery); 
+
+const countEl = document.getElementById('count');
+
+updateVisitCount();
+
+function updateVisitCount() {
+  fetch('https://api.countapi.xyz/update/perryfeinstein.com/8886648850?amount=1')
+  .then(res => res.json())
+  .then(res => {
+    countEl.innerHTML = res.value;
+  });
+}
